fe208aafe6419cf82e3c8e1fe1d743db465dac87
Jun Wang <junwang2003@gmail.com>
2024-12-04 21:30:12 -0600

    update package.json

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
-    baseUrl: "http://localhost:3000", // Adjust if needed
+    specPattern: "e2e/**/*.cy.{js,jsx,ts,tsx}",
+    //baseUrl: "http://localhost:3000", // Adjust if needed
   },
 });
