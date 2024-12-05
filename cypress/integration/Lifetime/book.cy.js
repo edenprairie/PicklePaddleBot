d918fed46d742fd289691f25c21f9e5089c7cd57
Jun Wang <junwang2003@gmail.com>
2024-12-04 21:46:39 -0600

    add debug

--- cypress/integration/Lifetime/book.cy.js
+++ cypress/integration/Lifetime/book.cy.js
@@ -1,31 +1,39 @@
 // cypress/integration/account_link_test.js
 
-describe('Verify Account Link and Prepare for Login', () => {
+describe('Book Pickleball court from Bloomington North', () => {
     it('Checks if the Account link is visible and clicks on it', () => {
         // Visit the target URL
         cy.visit('https://my.lifetime.life/clubs/mn/bloomington-north.html');
-
+        cy.log('line1');
         // Select the Account link by its visible text or class (adjust the selector as needed)
         cy.get('.nav-item > .nav-link > .js-login-label')
             .should('be.visible') // Verify it is visible
             .click(); // Click on the link to prepare for login
-
+        cy.log('line2');
         // Optionally, verify that the login form or page is displayed
         // For example:
         cy.get('.js-login-link').click();
+        cy.log('line3');
         cy.get('.login-username-group').type('junwang01@gmail.com');
+        cy.log('line4');
         cy.get('.login-password-group').type('EOt*1JUI67Sva$');
+        cy.log('line5');
         cy.get('.btn-wrap').click();
+        cy.log('line6');
         cy.get('#primaryNav > :nth-child(2) > .nav-link').click();
+        cy.log('line7');
         cy.get('.open > .dropdown-menu > .list-unstyled > :nth-child(2) > .dropdown-item').click();
+        cy.log('line8');
 
         //cy.get('.planner-date-btn-next').click();
         //cy.get(':nth-child(6) > [data-testid="plannerDate"] > .planner-date-day > .planner-date-number').click();
 
         cy.visit('https://my.lifetime.life/account/reservations.html?resourceId=ZXhlcnA6N2JyMzQwMTo3MDE1OTExNDE1MDg=&start=2024-12-08T20:00:00-06:00&duration=60&cancelUrl=aHR0cHM6Ly9teS5saWZldGltZS5saWZlL2NsdWJzL21uL2Jsb29taW5ndG9uLW5vcnRoL3Jlc291cmNlLWJvb2tpbmcuaHRtbD9zcG9ydD1QaWNrbGViYWxsJTNBK0luZG9vciZjbHViSWQ9NyZkYXRlPTIwMjQtMTItMDgmc3RhcnRUaW1lPS0xJmR1cmF0aW9uPTMwJmhpZGVNb2RhbD10cnVl');
+        cy.log('line9');
         cy.get('.c-indicator')
             .should('be.visible') // Verify it is visible
             .click();
+        cy.log('line10');
 
 
         //https://my.lifetime.life/account/reservations.html?resourceId=ZXhlcnA6N2JyMzYwMTo3MDE1OTExNDE1MDg=&start=2024-12-04T21:00:00-06:00&duration=60&cancelUrl=aHR0cHM6Ly9teS5saWZldGltZS5saWZlL2NsdWJzL21uL2Jsb29taW5ndG9uLW5vcnRoL3Jlc291cmNlLWJvb2tpbmcuaHRtbD9zcG9ydD1QaWNrbGViYWxsJTNBK0luZG9vciZjbHViSWQ9NyZkYXRlPTIwMjQtMTItMDQmc3RhcnRUaW1lPS0xJmR1cmF0aW9uPTYwJmhpZGVNb2RhbD10cnVl
@@ -39,3 +47,4 @@ describe('Verify Account Link and Prepare for Login', () => {
 
     });
 });
+
